8d6917d4fd7c1beed2553169e8a1f2ad940bafd0
Bbe <NNwws>
2026-04-05 16:28:30 -0400

    Reduce smooth scroll inertia

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -6,8 +6,8 @@ import Lenis from "lenis"
 export function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 0.7,
-      easing: (t) => 1 - Math.pow(1 - t, 2),
+      duration: 0.4,
+      easing: (t) => 1 - Math.pow(1 - t, 1.6),
       orientation: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,
